90462a0e792828e1aad25f32ffb21acb1d1d3db6
Maxim Shalunov <shalunoff-m@yandex.ru>
2018-09-06 17:58:12 +0300

    Начато построение сетки документа

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
                     style: "expanded"
                 },
                 files: {
-                    'css/style.css': 'style.scss'
+                    'css/style.css': 'style.scss',
                 },
 
             },
