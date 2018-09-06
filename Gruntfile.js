bdca56e68ea36914f530ef3705caf5dcda31974a
Maxim Shalunov <shalunoff-m@yandex.ru>
2018-09-06 15:01:24 +0300

    Добавлен grunt, sass, editorconfir. Первоначальная семантика index

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        //Конфигурация
+        sass: {
+            dev: {
+                option: {
+                    style: "expanded"
+                },
+                files: {
+                    'css/style.css': 'style.scss'
+                },
+
+            },
+
+        },
+        watch: {
+            sass: {
+                files: "*.scss",
+                tasks: "sass",
+
+            }
+
+        }
+
+
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['sass', 'watch']);
+
+    //Загрузка модулей
+
+
+
+
+};
